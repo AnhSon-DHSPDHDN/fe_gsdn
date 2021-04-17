3a154f5df3f30833efc713498593d704e0e291c1
AnhSon-DHSPDHDN <59197139+AnhSon-DHSPDHDN@users.noreply.github.com>
2021-04-17 09:34:17 +0700

    news dele

--- src/components/admins/NewsAdmin/index.js
+++ src/components/admins/NewsAdmin/index.js
@@ -21,18 +21,28 @@ export default function NewsAdmin() {
     let result = <tr><td></td></tr>;
     if (data) {
       result = data.map((data, index) => {
-        return <tr key={index}>
-          <td>{data?.title}</td>
-          <td>{data?.description}</td>
-          <td>{data?.link}</td>
+        return <tr key={index} className="row-table">
+          <td><img src={data?.image}></img></td>
+          <td><p>{data?.title}</p></td>
+          <td><p>{data?.description}</p></td>
+          <td><p>{data?.link}</p></td>
           <td>
             <Button>Sửa</Button>
-            <Button>Xóa</Button>
+            <Button danger onClick={() => handleClickDelete(data._id)}>Xóa</Button>
           </td>
         </tr>
       })
     }
     return result
+  }
+
+  const handleClickDelete = (id) => {
+    axiosClient.delete(`/news/${id}`)
+      .then(res => {
+        console.log(res.data);
+      }).catch(err => {
+        console.log(err);
+      })
   }
 
   useEffect(() => {
@@ -47,6 +57,7 @@ export default function NewsAdmin() {
         <table className="table table-hover">
           <thead>
             <tr>
+              <th>Image</th>
               <th>Title</th>
               <th>Description</th>
               <th>Link</th>
